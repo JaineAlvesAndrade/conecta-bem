7e1d2866fd24852467598d00bf20c78ad6ebb963
Victor Kruger <98160719+victormk2@users.noreply.github.com>
2026-03-09 19:41:47 -0300

    auth improvements

--- src/app/core/components/Auth/AuthComponent.ts
+++ src/app/core/components/Auth/AuthComponent.ts
@@ -1,31 +1,47 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslateModule, MatIconModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
   isLogin = true;
   name = '';
   email = '';
   password = '';
-  role = 'VOLUNTARIO';
+  confirmPassword = '';
+  showPassword = false;
+  showConfirm = false;
 
   toggleMode() {
     this.isLogin = !this.isLogin;
   }
 
   onSubmit() {
-    console.log('Dados enviados:', { 
-      email: this.email, 
-      password: this.password, 
-      name: this.name, 
-      role: this.role 
+    if (!this.isLogin && this.password !== this.confirmPassword) {
+      console.error('As senhas não coincidem');
+      return;
+    }
+
+    console.log('Dados enviados:', {
+      email: this.email,
+      password: this.password,
+      name: this.name,
     });
+  }
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleShowConfirm() {
+    this.showConfirm = !this.showConfirm;
   }
 }
